967090f526f12c1f7bee6c2b749804bc65b26784
Elly <ellyhuang26@gmail.com>
2023-11-20 08:21:11 +0800

    Optimized submit,Validation,Modal, with AJAX and Controller modification

--- SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Areas/Users/Controllers/HomeController.cs
+++ SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Areas/Users/Controllers/HomeController.cs
@@ -40,19 +40,58 @@ namespace sifoodprojectformal2._0.Areas.Users.Controllers
             return View();
         }
 
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+
+        ////Address>now only one input
+        //public IActionResult JoinUs([Bind("StoreName, ContactName,TaxID,Email,Phone,Address,Description,OpeningTime,OpeningDay")]JoinUsViewModel joinUsViewModel)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        return RedirectToAction("JoinUs");
+        //    }
+        //   return View(joinUsViewModel);
+        //}
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-
-        //Address>now only one input
-        public IActionResult JoinUs([Bind("StoreName, ContactName,TaxID,Email,Phone,Address,Description,OpeningTime,OpeningDay")]JoinUsViewModel joinUsViewModel)
+        public async Task<IActionResult> JoinUs([Bind("StoreName, ContactName,TaxID,Email,Phone,Address,Description,OpeningTime,OpeningDay")] JoinUsViewModel joinUsViewModel)
         {
             if (ModelState.IsValid)
             {
-                return RedirectToAction("JoinUs");
+                // 創建一個新的 Store 實例
+                var store = new Store
+                {
+                    StoreName = joinUsViewModel.StoreName,
+                    ContactName = joinUsViewModel.ContactName,
+                    TaxId = joinUsViewModel.TaxId,
+                    Email = joinUsViewModel.Email,
+                    Phone= joinUsViewModel.Phone,
+                    OpeningDay = joinUsViewModel.OpeningDay,
+                    OpeningTime = joinUsViewModel.OpeningTime,
+                    Address = joinUsViewModel.Address,
+                    Description = joinUsViewModel.Description,
+
+                };
+
+                // 將 store 實例添加到數據庫上下文的 Stores 集合中
+                _context.Stores.Add(store);
+
+                // 保存更改到數據庫
+                await _context.SaveChangesAsync();
+
+                // 返回 JSON 響應
+                return Json(new { success = true, message = "申請已成功提交！" });
             }
-           return View(joinUsViewModel);
+            else
+            {
+                // 返回包含錯誤信息的 JSON 響應
+                return Json(new { success = false, message = "表單驗證失敗，請檢查輸入內容。" });
+            }
         }
-        public IActionResult Products()
+
+        // 其他動作方法...
+    public IActionResult Products()
         {
             return View();
         }
@@ -62,3 +101,6 @@ namespace sifoodprojectformal2._0.Areas.Users.Controllers
         }
     }
 }
+ 
+
+   
