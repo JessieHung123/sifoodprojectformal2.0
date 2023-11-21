7bd7eb3510f9daeaf0e7481a4bb7761531153bd0
Elly <ellyhuang26@gmail.com>
2023-11-22 00:31:14 +0800

    update ProfileViewModel:Name&Error Message;modified 'lable for' and 'asp-for'  but failed to achieve the expected outcome

--- SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/ViewModels/Users/ProfileViewModel.cs
+++ SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/ViewModels/Users/ProfileViewModel.cs
@@ -4,20 +4,22 @@ namespace SiFoodProjectFormal2._0.ViewModels.Users
 {
     public class ProfileViewModel
     {
-        [Required(ErrorMessage = "請輸入用戶名")]
-        [Display(Name = "用戶名")]
+        [Required(ErrorMessage = "請輸入您的姓名")]
+        [Display(Name = "姓名")]
         public string UserName { get; set; }
 
-        [Required(ErrorMessage = "請輸入電子郵件")]
+        [Required(ErrorMessage = "電子郵件未填寫")]
         [EmailAddress(ErrorMessage = "請輸入有效的電子郵件地址")]
-        [Display(Name = "電子郵件")]
+        [Display(Name = "Email")]
         public string UserEmail { get; set; }
 
 
-
+        [Required(ErrorMessage = "聯絡電話未填寫")]
+        [RegularExpression(@"^09\d{8}$", ErrorMessage = "無效的手機號碼格式")]
         [Display(Name = "電話號碼")]
         public string UserPhone { get; set; }
 
+        [Required(ErrorMessage = "生日未填寫")]
         [Display(Name = "生日")]
         [DataType(DataType.Date)]
         public DateTime? UserBirthDate { get; set; }
