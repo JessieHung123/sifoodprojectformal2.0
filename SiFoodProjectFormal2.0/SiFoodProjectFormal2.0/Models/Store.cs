b3e8d1553667e808444de10f71b6b928eae0ab56
Brad <brad881112@gmail.com>
2023-11-21 19:41:50 +0800

    basic settings

--- SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Models/Store.cs
+++ SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Models/Store.cs
@@ -45,5 +45,7 @@ public partial class Store
 
     public virtual ICollection<Favorite> Favorites { get; set; } = new List<Favorite>();
 
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
 }
