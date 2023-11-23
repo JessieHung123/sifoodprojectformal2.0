332dd401de059fedb70b935c2597a04b3e733285
Simon <ivonneture@gmail.com>
2023-11-23 18:13:38 +0800

    vue StoreInfoApI update

--- SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Areas/Stores/Controllers/HomeController.cs
+++ SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Areas/Stores/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SiFoodProjectFormal2._0.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace sifoodprojectformal2._0.Areas.Stores.Controllers
 {
@@ -19,13 +20,13 @@ namespace sifoodprojectformal2._0.Areas.Stores.Controllers
         }
 
         // GET: Products
+        [HttpGet] //uri:/
         public async Task<IActionResult> Main()
         {
             //var sifoodContext = _context.Products.Include(p => p.Category).Include(p => p.Store).Where(p => p.Store.StoreId == targetStoreId);
             var sifoodContext = _context.Products.Where(p => p.StoreId == targetStoreId);
             string storeName = await _context.Stores.Where(s => s.StoreId == targetStoreId).Select(s => s.StoreName).FirstOrDefaultAsync();
-
-            int SumReleasedQty =  _context.Products.Where(p => p.StoreId == targetStoreId).Sum( p => p.OrderedQty);
+            int SumReleasedQty = _context.Products.Where(p => p.StoreId == targetStoreId).Sum(p => p.OrderedQty);
             //int ReleasedQty = await _context.Products.Where(od => od.StoreId == targetStoreId).Sum(od => od.ReleasedQty);
             int status1Count = await _context.Orders.CountAsync(od => od.StatusId == 1 && od.StoreId == targetStoreId);
             int status2Count = await _context.Orders.CountAsync(od => od.StatusId == 2 && od.StoreId == targetStoreId);
@@ -37,9 +38,29 @@ namespace sifoodprojectformal2._0.Areas.Stores.Controllers
             ViewBag.status3 = status3Count;
             ViewBag.status4 = status4Count;
             ViewBag.SumReleasedQty = SumReleasedQty;
-
             return View(await sifoodContext.ToListAsync());
         }
+
+        public async Task<IActionResult> Main2()
+        {
+            var sifoodContext2 = _context.OrderDetails.Include(d => d.Product).Select(x => new { StoreId = x.Product.StoreId, UnitPrice = x.Product.UnitPrice, OrderId = x.OrderId, ProductId = x.ProductId, ProductName = x.Product.ProductName }).Where(e => e.StoreId == targetStoreId);
+            return Json(sifoodContext2);
+
+        }
+
+        //public async Task<IActionResult> Main2()
+        //{
+        //    var sifoodContext2 = _context.OrderDetails.Include(d => d.Product).Select(x => new { StoreId = x.Product.StoreId, OrderDetailId = x.OrderDetailId, OrderId = x.OrderId, ProductId = x.ProductId, ProductName = x.Product.ProductName }).Where(e => e.StoreId == targetStoreId);
+        //    return Json(sifoodContext2);
+
+        //}
+
+        //public async Task<IActionResult> Main3()
+        //{
+        //    var sifoodContext3 = _context.Products.Select(x => new { StoreId = x.StoreId, Description = x.Description, ProductName = x.ProductName, ReleasedQty = x.ReleasedQty }).Where(p => p.StoreId == targetStoreId);
+        //    return Json(sifoodContext3);
+
+        //}
 
         //----------------------------------//
         //public IActionResult Main()
