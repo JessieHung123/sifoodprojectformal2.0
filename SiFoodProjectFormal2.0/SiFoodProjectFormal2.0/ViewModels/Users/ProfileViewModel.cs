68a6d2f24e14a5f469b5e39cf7ce2d8c6dd7202a
Elly <ellyhuang26@gmail.com>
2023-11-23 18:13:27 +0800

    Scaffold;ID先寫好取UserID U002可成功修改到資料庫;修改密碼註解中,待處理

--- SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/ViewModels/Users/ProfileViewModel.cs
+++ SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/ViewModels/Users/ProfileViewModel.cs
@@ -4,37 +4,40 @@ namespace SiFoodProjectFormal2._0.ViewModels.Users
 {
     public class ProfileViewModel
     {
-        [Required(ErrorMessage = "請輸入您的姓名")]
+
+        public string UserId { get; set; } = null!;
+
+       // [Required(ErrorMessage = "請輸入您的姓名")]
         [Display(Name = "姓名")]
         public string UserName { get; set; }
 
-        [Required(ErrorMessage = "電子郵件未填寫")]
-        [EmailAddress(ErrorMessage = "請輸入有效的電子郵件地址")]
+       // [Required(ErrorMessage = "電子郵件未填寫")]
+       // [EmailAddress(ErrorMessage = "請輸入有效的電子郵件地址")]
         [Display(Name = "Email")]
         public string UserEmail { get; set; }
 
 
-        [Required(ErrorMessage = "聯絡電話未填寫")]
-        [RegularExpression(@"^09\d{8}$", ErrorMessage = "無效的手機號碼格式")]
+       // [Required(ErrorMessage = "聯絡電話未填寫")]
+        //[RegularExpression(@"^09\d{8}$", ErrorMessage = "無效的手機號碼格式")]
         [Display(Name = "電話號碼")]
         public string UserPhone { get; set; }
 
-        [Required(ErrorMessage = "生日未填寫")]
-        [Display(Name = "生日")]
+       // [Required(ErrorMessage = "生日未填寫")]
+        //[Display(Name = "生日")]
         [DataType(DataType.Date)]
         public DateTime? UserBirthDate { get; set; }
 
-        [DataType(DataType.Password)]
-        [Display(Name = "當前密碼")]
-        public string CurrentPassword { get; set; }
+        //[DataType(DataType.Password)]
+        //[Display(Name = "當前密碼")]
+        //public string CurrentPassword { get; set; }
 
-        [DataType(DataType.Password)]
-        [Display(Name = "新密碼")]
-        public string NewPassword { get; set; }
+        //[DataType(DataType.Password)]
+        //[Display(Name = "新密碼")]
+        //public string NewPassword { get; set; }
 
-        [DataType(DataType.Password)]
-        [Display(Name = "確認新密碼")]
-        [Compare("NewPassword", ErrorMessage = "新密碼和確認密碼不匹配")]
-        public string ConfirmPassword { get; set; }
+        //[DataType(DataType.Password)]
+        //[Display(Name = "確認新密碼")]
+        //[Compare("NewPassword", ErrorMessage = "新密碼和確認密碼不匹配")]
+        //public string ConfirmPassword { get; set; }
     }
 }
