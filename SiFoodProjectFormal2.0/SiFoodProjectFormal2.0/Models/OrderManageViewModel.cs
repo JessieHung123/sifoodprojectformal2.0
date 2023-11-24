c459af3d7135c2498389db5a7a4f9effd3cf461c
Chanel <hsiangting724@gmail.com>
2023-11-24 11:07:12 +0800

    更新

---
+++ SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Models/OrderManageViewModel.cs
@@ -0,0 +1,23 @@
+﻿
+namespace SiFoodProjectFormal2._0.Models
+{
+    public class OrderManageViewModel
+    {
+        
+        public string? OrderAddress { get; set; }
+        public string? OrderId { get; set; } 
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public string? UserName { get; set; }
+        public int Quantity { get; set; }
+        public string? UserPhone { get; set; }
+        public int OrderDetailId { get; set; }
+        public ICollection<UserAddress>? UserAddress { get; internal set; }
+        public DateTime OrderDate { get; set; }       
+        public string? StatusName { get; internal set; }
+        public decimal ProductUnitPrice { get; internal set; }
+        public string StoreName { get; internal set; }
+        public string StorePhone { get; internal set; }
+        public string StoreAddress { get; internal set; }
+    }
+}
