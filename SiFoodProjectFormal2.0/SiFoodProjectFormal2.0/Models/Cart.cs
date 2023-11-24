cfafb5cea92c0dc3179ee091b8784dffb6d417b9
Brad <brad881112@gmail.com>
2023-11-24 11:25:01 +0800

    Update ScaffoldDb

--- SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Models/Cart.cs
+++ SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Models/Cart.cs
@@ -11,7 +11,11 @@ public partial class Cart
 
     public int Quantity { get; set; }
 
+    public string StoreId { get; set; } = null!;
+
     public virtual Product Product { get; set; } = null!;
+
+    public virtual Store Store { get; set; } = null!;
 
     public virtual User User { get; set; } = null!;
 }
