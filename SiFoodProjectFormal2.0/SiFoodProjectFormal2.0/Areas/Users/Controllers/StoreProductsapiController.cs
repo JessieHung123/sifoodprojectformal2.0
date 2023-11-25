973f29b3e8c179990ad6d345331e76f9699eb35e
Jay <cswy14@outlook.com>
2023-11-25 16:41:30 +0800

    add StoreProductsapiController

---
+++ SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Areas/Users/Controllers/StoreProductsapiController.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SiFoodProjectFormal2._0.Models;
+using SiFoodProjectFormal2._0.ViewModels.Users;
+
+namespace SiFoodProjectFormal2._0.Areas.Users.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoreProductsapiController : ControllerBase
+    {
+        private readonly Sifood3Context _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public StoreProductsapiController(Sifood3Context context, IWebHostEnvironment webHostEnvironment)
+        {
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        // GET: api/StoreProductsapi
+        [HttpGet]
+        public async Task<IEnumerable<Store>> GetStores()
+        {
+            return _context.Stores;
+        }
+
+        // GET: api/StoreProductsapi/5
+        [HttpGet("{id}")]
+        public object GetStore(string id)
+        {
+
+            var today = DateTime.Today;
+            var currentTime = DateTime.Now.TimeOfDay;
+            var store = _context.Stores.AsNoTracking().Include(x => x.Products).Include(x => x.Orders)
+               .ThenInclude(x => x.Comment).Where(c => c.StoreId == id);
+
+            return store.Select(z => new StoreProductsVM
+            {
+                StoreName = z.StoreName,
+                StoreId = z.StoreId,
+                Email = z.Email,
+                Phone = $"{z.Phone.Substring(0, 2)} {z.Phone.Substring(2)}",
+                Address = z.Address,
+                OpeningTime = z.OpeningTime,
+                PhotosPath = z.PhotosPath,
+                Description = z.Description,
+                LogoPath = z.LogoPath,
+                CommentCount = z.Orders.Where(x => x.Comment != null).Count(),
+                CommentRank = z.Orders.Sum(x => x.Comment.CommentRank),
+                WeekdayOpeningTime = z.OpeningTime.Substring(0, 16),
+                WeekendOpeningTime = z.OpeningTime.Substring(17, 16),
+
+                Products = z.Products.Where(p => p.RealeasedTime.Date == today && p.RealeasedTime.TimeOfDay < currentTime && p.SuggestPickEndTime > currentTime).Select(p => new ProductsVM
+                {
+                    UnitPrice = p.UnitPrice,
+                    ProductName = p.ProductName,
+                    CategoryId = p.CategoryId,
+                    CategoryName = p.Category.CategoryName,
+                    avalibleQty = p.ReleasedQty - p.OrderedQty,
+                    SuggestPickUpTime = $"{p.SuggestPickUpTime.ToString(@"hh\:mm")} ~ {p.SuggestPickEndTime.ToString(@"hh\:mm")}",
+                    RealeasedTime = p.RealeasedTime,
+                    PhotoPath = p.PhotoPath,
+                }),
+
+                CategoryList = z.Products.Where(p => p.RealeasedTime.Date == today && p.RealeasedTime.TimeOfDay < currentTime && p.SuggestPickEndTime > currentTime).Select(y => y.Category.CategoryName).Distinct().ToArray(),
+
+                Comment = z.Orders.Select(d => new CommentVM
+                {
+                    Contents = d.Comment.Contents,
+                    CommentRank = d.Comment.CommentRank,
+                    User = d.User.UserName,
+                    DeliveryMethod = d.DeliveryMethod
+                })
+            });
+
+        }
+
+        // PUT: api/StoreProductsapi/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStore(string id, Store store)
+        {
+            if (id != store.StoreId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(store).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StoreExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/StoreProductsapi
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Store>> PostStore(Store store)
+        {
+          if (_context.Stores == null)
+          {
+              return Problem("Entity set 'Sifood3Context.Stores'  is null.");
+          }
+            _context.Stores.Add(store);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetStore", new { id = store.StoreId }, store);
+        }
+
+        // DELETE: api/StoreProductsapi/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStore(string id)
+        {
+            if (_context.Stores == null)
+            {
+                return NotFound();
+            }
+            var store = await _context.Stores.FindAsync(id);
+            if (store == null)
+            {
+                return NotFound();
+            }
+
+            _context.Stores.Remove(store);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool StoreExists(string id)
+        {
+            return (_context.Stores?.Any(e => e.StoreId == id)).GetValueOrDefault();
+        }
+    }
+}
