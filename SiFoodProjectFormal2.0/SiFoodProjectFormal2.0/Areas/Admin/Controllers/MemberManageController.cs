d9237d1962f14bdab305605467a07af796565686
Albert <albertpcshen@gmail.com>
2023-11-28 13:26:13 +0800

    edit new modal

--- SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Areas/Admin/Controllers/MemberManageController.cs
+++ SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Areas/Admin/Controllers/MemberManageController.cs
@@ -19,145 +19,35 @@ namespace SiFoodProjectFormal2._0.Areas.Admin.Controllers
             _context = context;
         }
 
-        // GET: Admin/MemberManage
         public async Task<IActionResult> Index(string searchString)
         {
-            //return View(await _context.Users.ToListAsync());
-
             IQueryable<User> users = _context.Users;
             if (!string.IsNullOrEmpty(searchString))
             {
-                // 如果搜尋字串不為空，則使用 LINQ 過濾 users 變數
                 users = users.Where(u => u.UserName.Contains(searchString) || u.UserEmail.Contains(searchString) || u.UserPhone.Contains(searchString));
-               
             }
             return View(await users.ToListAsync());
-
-            //return _context.Users != null ? 
-            //View(await _context.Users.ToListAsync()) :
-            //Problem("Entity set 'Sifood3Context.Users'  is null.");
         }
 
-        // GET: Admin/MemberManage/Details/5
         public IActionResult Details1(string UserId)
         {
             User user = _context.Users.Where(x => x.UserId == UserId).FirstOrDefault();
             return PartialView("_Details", user);
         }
-        public async Task<IActionResult> Details(string UserId)
-        {
-            if (UserId == null || _context.Users == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _context.Users
-                .FirstOrDefaultAsync(m => m.UserId == UserId);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return View(user);
-        }
-
-        // GET: Admin/MemberManage/Create
-
-        //public IActionResult Create()
-        //{
-        //    User user = new User();
-        //    return PartialView("Create", user);
-        //}
-        //public IActionResult Create()
-        //{
-        //    return View();
-        //}
-
-        // POST: Admin/MemberManage/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Create([Bind("UserId,UserName,UserEmail,UserPasswordHash,UserPhone,UserBirthDate,TotalOrderAmount,UserEnrollDate,UserLatestLogInDate,UserPasswordSalt")] User user)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        _context.Add(user);
-        //        await _context.SaveChangesAsync();
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(user);
-        //}
-
-        // GET: Admin/MemberManage/Edit/5
-
-
+        
         public IActionResult Edit1(string UserId)
         {
             User user = _context.Users.Where(x => x.UserId == UserId).FirstOrDefault();
             return PartialView("_Edit", user);
         }
         
-        public async Task<IActionResult> Edit(string UserId)
-        {
-            if (UserId == null || _context.Users == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _context.Users.FindAsync(UserId);
-            if (user == null)
-            {
-                return NotFound();
-            }
-            return View(user);
-        }
-
-        // POST: Admin/MemberManage/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-
-        [HttpPost]
+        [HttpPost]       
         public IActionResult Edit1(User user)
         {
             _context.Users.Update(user);
-            _context.SaveChangesAsync();
+            _context.SaveChanges();
             return RedirectToAction("Index");
         }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string UserId, [Bind("UserId,UserName,UserEmail,UserPasswordHash,UserPhone,UserBirthDate,TotalOrderAmount,UserEnrollDate,UserLatestLogInDate,UserPasswordSalt")] User user)
-        {
-            if (UserId != user.UserId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(user);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!UserExists(user.UserId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(user);
-        }
-
-        // GET: Admin/MemberManage/Delete/5
 
         public IActionResult Delete1(string UserId)
         {
@@ -171,46 +61,6 @@ namespace SiFoodProjectFormal2._0.Areas.Admin.Controllers
             _context.Users.Remove(user);
             _context.SaveChanges();
             return RedirectToAction("Index");
-        }
-        public async Task<IActionResult> Delete(string UserId)
-        {
-            if (UserId == null || _context.Users == null)
-            {
-                return NotFound();
-            }
-
-            var user = await _context.Users
-                .FirstOrDefaultAsync(m => m.UserId == UserId);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            return View(user);
-        }
-
-        // POST: Admin/MemberManage/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string UserId)
-        {
-            if (_context.Users == null)
-            {
-                return Problem("Entity set 'Sifood3Context.Users'  is null.");
-            }
-            var user = await _context.Users.FindAsync(UserId);
-            if (user != null)
-            {
-                _context.Users.Remove(user);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool UserExists(string id)
-        {
-          return (_context.Users?.Any(e => e.UserId == id)).GetValueOrDefault();
         }
     }
 }
--- SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Areas/Admin/Controllers/StoreManageController.cs
+++ SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Areas/Admin/Controllers/StoreManageController.cs
@@ -18,146 +18,49 @@ namespace SiFoodProjectFormal2._0.Areas.Admin.Controllers
         {
             _context = context;
         }
-
-        // GET: Admin/StoreManage
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string searchString)
         {
-              return _context.Stores != null ? 
-                          View(await _context.Stores.ToListAsync()) :
-                          Problem("Entity set 'Sifood3Context.Stores'  is null.");
-        }
-
-        // GET: Admin/StoreManage/Details/5
-        public async Task<IActionResult> Details(string StoreId)
-        {
-            if (StoreId == null || _context.Stores == null)
+            IQueryable<Store> stores = _context.Stores;
+            if (!string.IsNullOrEmpty(searchString))
             {
-                return NotFound();
+                stores = stores.Where(u => u.StoreName.Contains(searchString));
             }
-
-            var store = await _context.Stores
-                .FirstOrDefaultAsync(m => m.StoreId == StoreId);
-            if (store == null)
-            {
-                return NotFound();
-            }
-
-            return View(store);
+            return View(await stores.ToListAsync());
         }
 
-        // GET: Admin/StoreManage/Create
-        public IActionResult Create()
+        public IActionResult Details(string storeId)
         {
-            return View();
+            Store store = _context.Stores.Where(x => x.StoreId == storeId).FirstOrDefault();
+            return PartialView("_DetailsPartialView", store);
         }
 
-        // POST: Admin/StoreManage/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        public IActionResult Edit(string storeId)
+        {
+            Store store = _context.Stores.Where(x => x.StoreId == storeId).FirstOrDefault();
+            return PartialView("_EditPartialView", store);
+        }
+
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("StoreId,StoreName,ContactName,TaxId,Email,Phone,City,Region,Address,Description,PasswordHash,PasswordSalt,EnrollDate,Latitude,Longitude,OpeningTime,OpeningDay,PhotosPath,LogoPath")] Store store)
+        public IActionResult Edit(Store store)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Add(store);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(store);
+            _context.Stores.Update(store);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
 
-        // GET: Admin/StoreManage/Edit/5
-        public async Task<IActionResult> Edit(string StoreId)
+        public IActionResult Delete(string storeId)
         {
-            if (StoreId == null || _context.Stores == null)
-            {
-                return NotFound();
-            }
-
-            var store = await _context.Stores.FindAsync(StoreId);
-            if (store == null)
-            {
-                return NotFound();
-            }
-            return View(store);
+            Store store = _context.Stores.Where(x => x.StoreId == storeId).FirstOrDefault();
+            return PartialView("_DeletePartialView", store);
         }
 
-        // POST: Admin/StoreManage/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(string StoreId, [Bind("StoreId,StoreName,ContactName,TaxId,Email,Phone,City,Region,Address,Description,PasswordHash,PasswordSalt,EnrollDate,Latitude,Longitude,OpeningTime,OpeningDay,PhotosPath,LogoPath")] Store store)
+        public IActionResult Delete(Store store)
         {
-            if (StoreId != store.StoreId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(store);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!StoreExists(store.StoreId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(store);
+            _context.Stores.Remove(store);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
 
-        // GET: Admin/StoreManage/Delete/5
-        public async Task<IActionResult> Delete(string StoreId)
-        {
-            if (StoreId == null || _context.Stores == null)
-            {
-                return NotFound();
-            }
-
-            var store = await _context.Stores
-                .FirstOrDefaultAsync(m => m.StoreId == StoreId);
-            if (store == null)
-            {
-                return NotFound();
-            }
-
-            return View(store);
-        }
-
-        // POST: Admin/StoreManage/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(string StoreId)
-        {
-            if (_context.Stores == null)
-            {
-                return Problem("Entity set 'Sifood3Context.Stores'  is null.");
-            }
-            var store = await _context.Stores.FindAsync(StoreId);
-            if (store != null)
-            {
-                _context.Stores.Remove(store);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool StoreExists(string StoreId)
-        {
-          return (_context.Stores?.Any(e => e.StoreId == StoreId)).GetValueOrDefault();
-        }
     }
 }
