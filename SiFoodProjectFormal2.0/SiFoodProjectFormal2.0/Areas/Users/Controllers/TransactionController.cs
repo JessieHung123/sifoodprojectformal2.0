9994b59f75cea9443ef0e971b3cf36f71b04af32
Brad <brad881112@gmail.com>
2023-11-29 16:08:32 +0800

    spgateway tryout

--- SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Areas/Users/Controllers/TransactionController.cs
+++ SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Areas/Users/Controllers/TransactionController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using SiFoodProjectFormal2._0.Areas.Users.Models.SPGatewayModels;
 using SiFoodProjectFormal2._0.Areas.Users.Models.ViewModels;
 using SiFoodProjectFormal2._0.Models;
 using SiFoodProjectFormal2._0.ViewModels.Users;
+using System.Text;
 
 namespace sifoodprojectformal2._0.Areas.Users.Controllers
 {
@@ -13,7 +15,16 @@ namespace sifoodprojectformal2._0.Areas.Users.Controllers
         public TransactionController(Sifood3Context context)
         {
             _context = context;
+
         }
+        private BankInfoModel _bankInfoModel = new BankInfoModel
+        {
+            MerchantID = "MS150834052",
+            HashKey = "EXyVmIxZL7uvX09Sk2wJp5hoDyGPocWn",
+            HashIV = "C09KA4wzx60UCOqP",
+            ReturnURL = "https://localhost:7042/Users/Home/RealTimeOrders",
+            AuthUrl = " https://ccore.newebpay.com/MPG/mpg_gateway",
+        };
 
         public IActionResult CheckOut()
         {
@@ -41,7 +52,7 @@ namespace sifoodprojectformal2._0.Areas.Users.Controllers
 
         [HttpPost]
         [Route("/Transaction/TakeOutOrder")]
-        public string TakeOutOrder([FromBody] CreateOrderVM model)
+        public IActionResult TakeOutOrder([FromBody] CreateOrderVM model)
         {
             string StoreId = _context.Stores.Where(s => s.StoreName == model.StoreName).Select(s => s.StoreId).Single();
             string UserId = _context.Users.Where(x => x.UserName == model.UserName).Select(x => x.UserId).Single();
@@ -55,12 +66,10 @@ namespace sifoodprojectformal2._0.Areas.Users.Controllers
                 TotalPrice = model.TotalPrice
             };
             _context.Orders.Add(order);
-            _context.SaveChanges();
 
             foreach (var item in model.ProductDetails)
             {
                 int productId = GetProductIdByName(item.ProductName);
-
                 OrderDetail orderDetail = new OrderDetail
                 {
                     OrderId = order.OrderId,
@@ -68,11 +77,95 @@ namespace sifoodprojectformal2._0.Areas.Users.Controllers
                     Quantity = item.Quantity,
                 };
                 _context.OrderDetails.Add(orderDetail);
-                _context.SaveChanges();
-
             }
-            return "訂單下訂成功!";
+            _context.SaveChanges();
+
+
+
+            string version = "2";
+            // 目前時間轉換 +08:00, 防止傳入時間或Server時間時區不同造成錯誤
+            DateTimeOffset taipeiStandardTimeOffset = DateTimeOffset.Now.ToOffset(new TimeSpan(8, 0, 0));
+            TradeInfo tradeInfo = new TradeInfo()
+            {
+                // * 商店代號
+                MerchantID = _bankInfoModel.MerchantID,
+
+                // * 回傳格式
+                RespondType = "String",
+
+                // * TimeStamp
+                TimeStamp = taipeiStandardTimeOffset.ToUnixTimeSeconds().ToString(),
+
+                // * 串接程式版本
+                Version = version,
+
+                // * 商店訂單編號
+                //MerchantOrderNo = $"T{DateTime.Now.ToString("yyyyMMddHHmm")}",
+                MerchantOrderNo = order.OrderId,
+
+                // * 訂單金額
+                Amt = model.TotalPrice,
+
+                // * 商品資訊
+                ItemDesc = "商品資訊(自行修改)",
+
+                // 支付完成 返回商店網址
+                ReturnURL = _bankInfoModel.ReturnURL,
+
+                // * 付款人電子信箱
+                Email = string.Empty,
+                // 付款人電子信箱 是否開放修改(1=可修改 0=不可修改)
+                EmailModify = 1,
+
+                // 信用卡 一次付清啟用(1=啟用、0或者未有此參數=不啟用)
+                CREDIT = 1,
+            };
+
+            Atom<string> result = new Atom<string>()
+            {
+                IsSuccess = true
+            };
+
+            var inputModel = new SpgatewayInputModel
+            {
+                MerchantID = _bankInfoModel.MerchantID,
+                Version = version
+            };
+
+            // 將model 轉換為List<KeyValuePair<string, string>>, null值不轉
+            List<KeyValuePair<string, string>> tradeData = LambdaUtil.ModelToKeyValuePairList<TradeInfo>(tradeInfo);
+            // 將List<KeyValuePair<string, string>> 轉換為 key1=Value1&key2=Value2&key3=Value3...
+            var tradeQueryPara = string.Join("&", tradeData.Select(x => $"{x.Key}={x.Value}"));
+            // AES 加密
+            inputModel.TradeInfo = CryptoUtil.EncryptAESHex(tradeQueryPara, _bankInfoModel.HashKey, _bankInfoModel.HashIV);
+            // SHA256 加密
+            inputModel.TradeSha = CryptoUtil.EncryptSHA256($"HashKey={_bankInfoModel.HashKey}&{inputModel.TradeInfo}&HashIV={_bankInfoModel.HashIV}");
+
+            // 將model 轉換為List<KeyValuePair<string, string>>, null值不轉
+            List<KeyValuePair<string, string>> postData = LambdaUtil.ModelToKeyValuePairList<SpgatewayInputModel>(inputModel);
+
+            Response.Clear();
+
+            StringBuilder s = new StringBuilder();
+            s.Append("<html>");
+            s.AppendFormat("<body onload='document.forms[\"form\"].submit()'>");
+            s.AppendFormat("<form name='form' action='{0}' method='post'>", _bankInfoModel.AuthUrl);
+            foreach (KeyValuePair<string, string> item in postData)
+            {
+                s.AppendFormat("<input type='hidden' name='{0}' value='{1}' />", item.Key, item.Value);
+            }
+
+            s.Append("</form></body></html>");
+            Response.WriteAsync(s.ToString());
+            HttpContext.Response.CompleteAsync();
+
+            return Content(string.Empty);
         }
+
+
+
+
+
 
         private int GetProductIdByName(string productName)
         {
---
+++ SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Areas/Users/Models/SPGatewayModels/TradeInfo.cs
@@ -0,0 +1,54 @@
+﻿namespace SiFoodProjectFormal2._0.Areas.Users.Models.SPGatewayModels
+{
+    public class TradeInfo
+    {
+        public string? MerchantID { get; set; }
+
+        /// <summary>
+        /// * 回傳格式
+        /// <para>JSON 或是 String</para>
+        /// </summary>
+        public string? RespondType { get; set; }
+
+        /// <summary>
+        /// * TimeStamp
+        /// <para>自從 Unix 纪元（格林威治時間 1970 年 1 月 1 日 00:00:00）到當前時間的秒數。</para>
+        /// </summary>
+        public string? TimeStamp { get; set; }
+
+        /// <summary>
+        /// * 串接程式版本
+        /// </summary>
+        public string? Version { get; set; }
+
+        /// <summary>
+        /// * 商店訂單編號
+        /// <para>限英、數字、_ 格式</para>
+        /// <para>長度限制為20字</para>
+        /// <para>同一商店中此編號不可重覆。</para>
+        /// </summary>
+        public string? MerchantOrderNo { get; set; }
+
+        /// <summary>
+        /// * 訂單金額
+        /// </summary>
+        public int Amt { get; set; }
+
+        /// <summary>
+        /// * 商品資訊
+        /// <para>1.限制長度為50字。</para>
+        /// <para>2.編碼為Utf-8格式。</para>
+        /// <para>3.請勿使用斷行符號、單引號等特殊符號，避免無法顯示完整付款頁面。</para>
+        /// <para>4.若使用特殊符號，系統將自動過濾。</para>
+        /// </summary>
+        public string? ItemDesc { get; set; }
+
+        public string? ReturnURL { get; set; }
+
+        public string? Email { get; set; }
+
+        public int EmailModify { get; set; }
+
+        public int? CREDIT { get; set; }
+    }
+}
