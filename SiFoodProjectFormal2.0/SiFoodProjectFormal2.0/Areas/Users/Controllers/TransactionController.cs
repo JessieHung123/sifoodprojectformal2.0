c6d412ec8ca4b7038d151874e0a73efb15c8d5aa
Brad <brad881112@gmail.com>
2023-11-29 22:51:53 +0800

    final change before merge

--- SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Areas/Users/Controllers/TransactionController.cs
+++ SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Areas/Users/Controllers/TransactionController.cs
@@ -52,7 +52,7 @@ namespace sifoodprojectformal2._0.Areas.Users.Controllers
 
         [HttpPost]
         [Route("/Transaction/TakeOutOrder")]
-        public IActionResult TakeOutOrder([FromBody] CreateOrderVM model)
+        public string TakeOutOrder([FromBody] CreateOrderVM model)
         {
             string StoreId = _context.Stores.Where(s => s.StoreName == model.StoreName).Select(s => s.StoreId).Single();
             string UserId = _context.Users.Where(x => x.UserName == model.UserName).Select(x => x.UserId).Single();
@@ -81,86 +81,76 @@ namespace sifoodprojectformal2._0.Areas.Users.Controllers
             }
             _context.SaveChanges();
 
-            string version = "2";
-            // 目前時間轉換 +08:00, 防止傳入時間或Server時間時區不同造成錯誤
-            DateTimeOffset taipeiStandardTimeOffset = DateTimeOffset.Now.ToOffset(new TimeSpan(8, 0, 0));
-            TradeInfo tradeInfo = new TradeInfo()
-            {
-                // * 商店代號
-                MerchantID = _bankInfoModel.MerchantID,
+            return "訂單下訂成功!";
 
-                // * 回傳格式
-                RespondType = "String",
+            //string version = "2";
+            //// 目前時間轉換 +08:00, 防止傳入時間或Server時間時區不同造成錯誤
+            //DateTimeOffset taipeiStandardTimeOffset = DateTimeOffset.Now.ToOffset(new TimeSpan(8, 0, 0));
+            //TradeInfo tradeInfo = new TradeInfo()
+            //{
+            //    // * 商店代號
+            //    MerchantID = _bankInfoModel.MerchantID,
+            //    // * 回傳格式
+            //    RespondType = "String",
+            //    // * TimeStamp
+            //    TimeStamp = taipeiStandardTimeOffset.ToUnixTimeSeconds().ToString(),
+            //    // * 串接程式版本
+            //    Version = version,
+            //    // * 商店訂單編號
+            //    //MerchantOrderNo = $"T{DateTime.Now.ToString("yyyyMMddHHmm")}",
+            //    MerchantOrderNo = $"{order.OrderId}",
+            //    // * 訂單金額
+            //    Amt = model.TotalPrice,
+            //    // * 商品資訊
+            //    ItemDesc = "商品資訊(自行修改)",
+            //    // 支付完成 返回商店網址
+            //    ReturnURL = _bankInfoModel.ReturnURL,
 
-                // * TimeStamp
-                TimeStamp = taipeiStandardTimeOffset.ToUnixTimeSeconds().ToString(),
+            //    NotifyURL = _bankInfoModel.NotifyURL,
+            //    // * 付款人電子信箱
+            //    Email = string.Empty,
+            //    // 付款人電子信箱 是否開放修改(1=可修改 0=不可修改)
+            //    EmailModify = 1,
+            //    // 信用卡 一次付清啟用(1=啟用、0或者未有此參數=不啟用)
+            //    CREDIT = 1,
+            //};
+            //Atom<string> result = new Atom<string>()
+            //{
+            //    IsSuccess = true
+            //};
+            //var inputModel = new SpgatewayInputModel
+            //{
+            //    MerchantID = _bankInfoModel.MerchantID,
+            //    Version = version
+            //};
+            //// 將model 轉換為List<KeyValuePair<string, string>>, null值不轉
+            //List<KeyValuePair<string, string>> tradeData = LambdaUtil.ModelToKeyValuePairList<TradeInfo>(tradeInfo);
+            //// 將List<KeyValuePair<string, string>> 轉換為 key1=Value1&key2=Value2&key3=Value3...
+            //var tradeQueryPara = string.Join("&", tradeData.Select(x => $"{x.Key}={x.Value}"));
+            //// AES 加密
+            //inputModel.TradeInfo = CryptoUtil.EncryptAESHex(tradeQueryPara, _bankInfoModel.HashKey, _bankInfoModel.HashIV);
+            //// SHA256 加密
+            //inputModel.TradeSha = CryptoUtil.EncryptSHA256($"HashKey={_bankInfoModel.HashKey}&{inputModel.TradeInfo}&HashIV={_bankInfoModel.HashIV}");
 
-                // * 串接程式版本
-                Version = version,
+            //// 將model 轉換為List<KeyValuePair<string, string>>, null值不轉
+            //List<KeyValuePair<string, string>> postData = LambdaUtil.ModelToKeyValuePairList<SpgatewayInputModel>(inputModel);
 
-                // * 商店訂單編號
-                //MerchantOrderNo = $"T{DateTime.Now.ToString("yyyyMMddHHmm")}",
-                MerchantOrderNo = $"{order.OrderId}",
+            //Response.Clear();
 
-                // * 訂單金額
-                Amt = model.TotalPrice,
+            //StringBuilder s = new StringBuilder();
+            //s.Append("<html>");
+            //s.AppendFormat("<body onload='document.forms[\"form\"].submit()'>");
+            //s.AppendFormat("<form name='form' action='{0}' method='post'>", _bankInfoModel.AuthUrl);
+            //foreach (KeyValuePair<string, string> item in postData)
+            //{
+            //    s.AppendFormat("<input type='hidden' name='{0}' value='{1}' />", item.Key, item.Value);
+            //}
 
-                // * 商品資訊
-                ItemDesc = "商品資訊(自行修改)",
+            //s.Append("</form></body></html>");
+            //Response.WriteAsync(s.ToString());
+            //HttpContext.Response.CompleteAsync();
 
-                // 支付完成 返回商店網址
-                ReturnURL = _bankInfoModel.ReturnURL,
-
-                NotifyURL = _bankInfoModel.NotifyURL,
-
-                // * 付款人電子信箱
-                Email = string.Empty,
-                // 付款人電子信箱 是否開放修改(1=可修改 0=不可修改)
-                EmailModify = 1,
-
-                // 信用卡 一次付清啟用(1=啟用、0或者未有此參數=不啟用)
-                CREDIT = 1,
-            };
-
-            Atom<string> result = new Atom<string>()
-            {
-                IsSuccess = true
-            };
-
-            var inputModel = new SpgatewayInputModel
-            {
-                MerchantID = _bankInfoModel.MerchantID,
-                Version = version
-            };
-
-            // 將model 轉換為List<KeyValuePair<string, string>>, null值不轉
-            List<KeyValuePair<string, string>> tradeData = LambdaUtil.ModelToKeyValuePairList<TradeInfo>(tradeInfo);
-            // 將List<KeyValuePair<string, string>> 轉換為 key1=Value1&key2=Value2&key3=Value3...
-            var tradeQueryPara = string.Join("&", tradeData.Select(x => $"{x.Key}={x.Value}"));
-            // AES 加密
-            inputModel.TradeInfo = CryptoUtil.EncryptAESHex(tradeQueryPara, _bankInfoModel.HashKey, _bankInfoModel.HashIV);
-            // SHA256 加密
-            inputModel.TradeSha = CryptoUtil.EncryptSHA256($"HashKey={_bankInfoModel.HashKey}&{inputModel.TradeInfo}&HashIV={_bankInfoModel.HashIV}");
-
-            // 將model 轉換為List<KeyValuePair<string, string>>, null值不轉
-            List<KeyValuePair<string, string>> postData = LambdaUtil.ModelToKeyValuePairList<SpgatewayInputModel>(inputModel);
-
-            Response.Clear();
-
-            StringBuilder s = new StringBuilder();
-            s.Append("<html>");
-            s.AppendFormat("<body onload='document.forms[\"form\"].submit()'>");
-            s.AppendFormat("<form name='form' action='{0}' method='post'>", _bankInfoModel.AuthUrl);
-            foreach (KeyValuePair<string, string> item in postData)
-            {
-                s.AppendFormat("<input type='hidden' name='{0}' value='{1}' />", item.Key, item.Value);
-            }
-
-            s.Append("</form></body></html>");
-            Response.WriteAsync(s.ToString());
-            HttpContext.Response.CompleteAsync();
-
-            return Content(string.Empty);
+            //return Content(string.Empty);
         }
 
         private int GetProductIdByName(string productName)
--- SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Models/Sifood3Context.cs
+++ SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Models/Sifood3Context.cs
@@ -15,6 +15,8 @@ public partial class Sifood3Context : DbContext
     {
     }
 
+    public virtual DbSet<Admin> Admins { get; set; }
+
     public virtual DbSet<Cart> Carts { get; set; }
 
     public virtual DbSet<Category> Categories { get; set; }
@@ -41,10 +43,22 @@ public partial class Sifood3Context : DbContext
 
     public virtual DbSet<UserAddress> UserAddresses { get; set; }
 
-
+   
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.UseCollation("Chinese_Taiwan_Stroke_CI_AS");
+
+        modelBuilder.Entity<Admin>(entity =>
+        {
+            entity
+                .HasNoKey()
+                .ToTable("Admin");
+
+            entity.Property(e => e.Account).HasMaxLength(30);
+            entity.Property(e => e.Id).ValueGeneratedOnAdd();
+            entity.Property(e => e.Name).HasMaxLength(30);
+            entity.Property(e => e.Password).HasMaxLength(30);
+        });
 
         modelBuilder.Entity<Cart>(entity =>
         {
@@ -62,7 +76,6 @@ public partial class Sifood3Context : DbContext
 
             entity.HasOne(d => d.Product).WithMany(p => p.Carts)
                 .HasForeignKey(d => d.ProductId)
-                .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Cart_Products");
 
             entity.HasOne(d => d.User).WithMany(p => p.Carts)
@@ -223,7 +236,6 @@ public partial class Sifood3Context : DbContext
 
             entity.HasOne(d => d.Product).WithMany(p => p.OrderDetails)
                 .HasForeignKey(d => d.ProductId)
-                .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_OrderDetails_Products");
         });
 
@@ -254,6 +266,7 @@ public partial class Sifood3Context : DbContext
             entity.Property(e => e.ProductId).HasColumnName("ProductID");
             entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
             entity.Property(e => e.Description).HasMaxLength(15);
+            entity.Property(e => e.IsDelete).HasDefaultValueSql("((1))");
             entity.Property(e => e.PhotoPath).HasMaxLength(50);
             entity.Property(e => e.ProductName).HasMaxLength(15);
             entity.Property(e => e.RealeasedTime).HasColumnType("datetime");
@@ -265,12 +278,10 @@ public partial class Sifood3Context : DbContext
 
             entity.HasOne(d => d.Category).WithMany(p => p.Products)
                 .HasForeignKey(d => d.CategoryId)
-                .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Products_Category1");
 
             entity.HasOne(d => d.Store).WithMany(p => p.Products)
                 .HasForeignKey(d => d.StoreId)
-                .OnDelete(DeleteBehavior.ClientSetNull)
                 .HasConstraintName("FK_Products_Stores");
         });
 
