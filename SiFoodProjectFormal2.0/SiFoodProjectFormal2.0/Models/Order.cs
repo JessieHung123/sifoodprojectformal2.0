eb6f91b617fa59174ca0f9520c9f4d74fd423096
Elly <ellyhuang26@gmail.com>
2023-11-30 10:29:58 +0800

    scaffold

--- SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Models/Order.cs
+++ SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Models/Order.cs
@@ -9,23 +9,25 @@ public partial class Order
 
     public DateTime OrderDate { get; set; }
 
-    public string Address { get; set; } = null!;
+    public string? Address { get; set; }
 
     public string StoreId { get; set; } = null!;
 
-    public string DriverId { get; set; } = null!;
+    public string? DriverId { get; set; }
 
     public string UserId { get; set; } = null!;
 
     public string DeliveryMethod { get; set; } = null!;
 
-    public decimal ShippingFee { get; set; }
+    public decimal? ShippingFee { get; set; }
 
     public int StatusId { get; set; }
 
+    public int? TotalPrice { get; set; }
+
     public virtual Comment? Comment { get; set; }
 
-    public virtual Driver Driver { get; set; } = null!;
+    public virtual Driver? Driver { get; set; }
 
     public virtual ICollection<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
 
