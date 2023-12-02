fdf6cc3e5c948b504157945bae4028044084eb37
Jessie <yuchinghung890403@gmail.com>
2023-12-02 12:45:36 +0800

    create deliveyVM

---
+++ SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Areas/Drivers/Controllers/DeliveryOrderapiController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SiFoodProjectFormal2._0.Areas.Drivers.NewFolder3;
+using SiFoodProjectFormal2._0.Models;
+
+namespace SiFoodProjectFormal2._0.Areas.Drivers.Controllers
+{
+    [Route("api/DeliveryOrderapi/[action]")]
+
+    [Area("Drivers")]
+    public class DeliveryOrderapiController:Controller
+    {
+        private readonly Sifood3Context _context;
+
+        public DeliveryOrderapiController(Sifood3Context context)
+        {
+            _context = context;
+
+        }
+        public object WaitForDeliveryOrderSimple()
+        {
+
+            //Distance 前端算後端算?
+            return _context.Orders.Where(o => o.StatusId == 2).Include(o => o.User).Include(o => o.Store).Select(o => new DeliveryOrderVM
+            {
+                OrderId = o.OrderId,
+                OrderDate = o.OrderDate.ToString(),
+                Address = o.Address,
+                StoreName = o.Store.StoreName,
+                UserName = o.User.UserName,
+
+            });
+
+            
+        }
+        //public object WaitForDeliveryOrderDetails()
+        //{
+        //    //return _context.Orders.Where(o => o.StatusId == 2).Include(o => o.User).Include(o => o.Store).Include(x => x.OrderDetails).ThenInclude(x => x.Product).Select(o => new
+
+        //    //)
+        //}
+
+    }
+}
