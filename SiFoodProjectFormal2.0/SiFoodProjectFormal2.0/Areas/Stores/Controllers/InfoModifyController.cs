c24529171161abd0fa9afa69b44351215816a6d4
Simon <ivonneture@gmail.com>
2023-12-04 03:48:02 +0800

    fetch成功取得資料

---
+++ SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Areas/Stores/Controllers/InfoModifyController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SiFoodProjectFormal2._0.Areas.Stores.ViewModels;
+using SiFoodProjectFormal2._0.Models;
+using System.Drawing;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SiFoodProjectFormal2._0.Areas.Stores.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class InfoModifyController : ControllerBase
+    {
+        string targetStoreId = "S001";
+        private readonly Sifood3Context _context;
+
+        public InfoModifyController(Sifood3Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<InfoModifyVM>> GetAll()
+        {
+            return await _context.Stores
+                .Where(e => e.StoreId == targetStoreId).Select(x => new InfoModifyVM
+                {
+                    StoreId = x.StoreId,
+                    StoreName = x.StoreName,
+                    ContactName = x.ContactName,
+                    Email = x.Email,
+                    Phone = x.Phone,
+                    City = x.City,
+                    Region = x.Region,
+                    Address = x.Address,
+                    Description = x.Description,
+                    OpeningTime = x.OpeningTime,
+                    ClosingDay = x.ClosingDay,
+                    PhotosPath = x.PhotosPath,
+                    LogoPath = x.LogoPath,
+                    PhotosPath2 = x.PhotosPath2,
+                    PhotosPath3 = x.PhotosPath3
+                }).ToListAsync();
+        }
+    }
+
+
+
+
+}
