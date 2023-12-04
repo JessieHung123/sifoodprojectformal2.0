755086ea5b126613a4bdcd39361e4ea2fa0a6324
Jessie <yuchinghung890403@gmail.com>
2023-12-04 15:08:36 +0800

    註解不要的樣式

--- SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Areas/Users/Controllers/HomeController.cs
+++ SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Areas/Users/Controllers/HomeController.cs
@@ -30,7 +30,7 @@ namespace sifoodprojectformal2._0.Areas.Users.Controllers
         }
         [HttpGet]
 
-        public IActionResult UserFAQ()
+        public IActionResult FAQ()
         {
             return View();
 
