a613c87d135c0143c228829ef6100e6962ed1aad
Simon <ivonneture@gmail.com>
2023-12-05 04:23:54 +0800

    updatestore方法測試前的儲存

--- SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Areas/Stores/Controllers/InfoModifyController.cs
+++ SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Areas/Stores/Controllers/InfoModifyController.cs
@@ -44,9 +44,107 @@ namespace SiFoodProjectFormal2._0.Areas.Stores.Controllers
                     PhotosPath3 = x.PhotosPath3
                 }).ToListAsync();
         }
+
+        //存到店家logo的路徑
+        private async Task<string> SaveLogo(IFormFile photo)
+        {
+            if (photo != null)
+            {
+                var fileName = Path.GetFileName(photo.FileName);
+                var savePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/Stores/logo", fileName);
+
+                using (var stream = new FileStream(savePath, FileMode.Create))
+                {
+                    await photo.CopyToAsync(stream);
+                }
+
+                return $"/images/Stores/logo/{fileName}";
+            }
+            return null;
+        }
+        //存到店家照片的路徑
+        private async Task<string> SavePhoto(IFormFile photo)
+        {
+            if (photo != null)
+            {
+                var fileName = Path.GetFileName(photo.FileName);
+                var savePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/Stores/photo", fileName);
+
+                using (var stream = new FileStream(savePath, FileMode.Create))
+                {
+                    await photo.CopyToAsync(stream);
+                }
+
+                return $"/images/Stores/photo/{fileName}";
+            }
+            return null;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<string> PutInfoModify(string id, [FromForm] PutInfoModifyVM putInfoModifyVM)
+        {
+
+            if (id != putInfoModifyVM.StoreId)
+            {
+                return "修改商品失敗!";
+            }
+            Store store = await _context.Stores.FindAsync(id);
+            store.StoreName = putInfoModifyVM.StoreName;
+            store.ContactName = putInfoModifyVM.ContactName;
+            store.Email = putInfoModifyVM.Email;
+            store.Phone = putInfoModifyVM.Phone;
+            store.City = putInfoModifyVM.City;
+            store.Region = putInfoModifyVM.Region;
+            store.Address = putInfoModifyVM.Address;
+            store.Description = putInfoModifyVM.Description;
+            store.OpeningTime = putInfoModifyVM.OpeningTime;
+            store.ClosingDay = putInfoModifyVM.ClosingDay;
+            if (putInfoModifyVM.LogoImageFile != null)
+            {
+                store.LogoPath = await SaveLogo(putInfoModifyVM.LogoImageFile);
+            }
+            if (putInfoModifyVM.PhotoImageFile1 != null)
+            {
+                store.PhotosPath = await SavePhoto(putInfoModifyVM.PhotoImageFile1);
+            }
+            if (putInfoModifyVM.PhotoImageFile2 != null)
+            {
+                store.PhotosPath2 = await SavePhoto(putInfoModifyVM.PhotoImageFile2);
+            }
+            if (putInfoModifyVM.PhotoImageFile3 != null)
+            {
+                store.PhotosPath3 = await SavePhoto(putInfoModifyVM.PhotoImageFile3);
+            }
+
+            _context.Entry(store).State = EntityState.Modified;
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return "修改店家資料失敗!";
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return "修改店家資料成功!";
+
+
+        }
+
+        private bool ProductExists(string storeId)
+        {
+            return (_context.Stores?.Any(e => e.StoreId == storeId)).GetValueOrDefault();
+        }
+
+
+
+
     }
-
-
-
 
 }
