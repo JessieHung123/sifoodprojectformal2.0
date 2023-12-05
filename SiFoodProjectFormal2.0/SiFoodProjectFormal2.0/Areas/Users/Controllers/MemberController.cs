b32a7b0a4380f264c3385a1a8dbba200251c61b0
Elly <ellyhuang26@gmail.com>
2023-12-06 00:23:11 +0800

    Card

--- SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Areas/Users/Controllers/MemberController.cs
+++ SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Areas/Users/Controllers/MemberController.cs
@@ -19,7 +19,7 @@ namespace sifoodprojectformal2._0.Areas.Users.Controllers
 
         //11/23新版
         [HttpGet]
-        [Route("/Member/Profile")]
+        
         public async Task<IActionResult> Profile()
         {
             //if (id == null || _context.Users == null)
@@ -181,7 +181,7 @@ namespace sifoodprojectformal2._0.Areas.Users.Controllers
             return View(historyOrders);
         }
 
-        [Route("/Member/Favorite")]
+       
         public async Task<IActionResult> Favorite()
         {
             // var userId = "當前用戶的ID"; // 從用戶會話或身份驗證系統獲取
