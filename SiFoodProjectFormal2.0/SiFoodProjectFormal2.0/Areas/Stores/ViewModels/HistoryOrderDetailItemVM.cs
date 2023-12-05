50068aeb99034a070534716321db291737581172
Elly <ellyhuang26@gmail.com>
2023-12-05 18:33:23 +0800

    功能Done

---
+++ SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/Areas/Stores/ViewModels/HistoryOrderDetailItemVM.cs
@@ -0,0 +1,13 @@
+﻿namespace SiFoodProjectFormal2._0.Areas.Stores.ViewModels
+{
+    public class HistoryOrderDetailItemVM
+    {
+        public string PhotoPath { get; set; }
+        public string ProductName { get; set; }
+
+
+        public decimal UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public decimal SubTotal => UnitPrice * Quantity;
+    }
+}
