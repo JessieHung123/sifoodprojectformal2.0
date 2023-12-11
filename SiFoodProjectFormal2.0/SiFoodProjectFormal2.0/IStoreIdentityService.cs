b64e2c6faec00965ecebae6ef5d349a98828b6a7
Jessie <yuchinghung890403@gmail.com>
2023-12-11 16:47:26 +0800

    delete note

--- SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/IStoreIdentityService.cs
+++ SiFoodProjectFormal2.0/SiFoodProjectFormal2.0/IStoreIdentityService.cs
@@ -18,10 +18,7 @@ namespace SiFoodProjectFormal2._0
 
         public string GetStoreId()
         {
-            // 尝试获取指定的 Claim
             Claim? store = _httpContextAccessor.HttpContext?.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Name);
-
-            // 如果找到了 Claim，则返回其值；否则返回 null
             return store?.Value;
         }
     }
